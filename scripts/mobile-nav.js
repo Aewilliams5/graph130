061268b0301bb0902aa067720d1130122db37d00
Aewilliams5 <147776501+Aewilliams5@users.noreply.github.com>
2023-11-30 15:30:51 -0800

    new

---
+++ scripts/mobile-nav.js
@@ -0,0 +1,5 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks =  document.getElementsByClassName('navbar-link')[0]
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
